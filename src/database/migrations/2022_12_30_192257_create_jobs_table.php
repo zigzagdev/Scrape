749e8e2bf23b58d45d9bb8b427cb488425741039
zigzagdev <msts.oo0131@gmail.com>
2022-12-30 20:01:21 +0900

    migrationFile_add

--- src/database/migrations/2022_12_30_192257_create_jobs_table.php
+++ src/database/migrations/2022_12_30_192257_create_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJobCategoriesTable extends Migration
+class CreateJobsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,12 @@ class CreateJobCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('job_categories', function (Blueprint $table) {
+        Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255)->unique();
+            $table->text('description');
+            $table->unsignedBigInteger('category_id')->comment('category_id');
+            $table->foreign('category_id')->references('id')->on('jobs')->onUpdate('CASCADE')->onDelete('CASCADE');
             $table->timestamps();
         });
     }
@@ -26,6 +30,6 @@ class CreateJobCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('job_categories');
+        Schema::dropIfExists('jobs');
     }
 }
