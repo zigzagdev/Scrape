4cfbe63510fc978aebfea169703adda830e08b3e
zigzagdev <msts.oo0131@gmail.com>
2023-01-09 18:55:15 +0900

    factoryDummyData_inserted

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use Illuminate\Database\Seeder;
-use App\Models\Job;
+use App\Models\Book;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,17 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('job_categories')-> insert([
-            ['id' => 1, 'name' => '事業開発'],
-            ['id' => 2, 'name' => 'マーケティング'],
-            ['id' => 3, 'name' => '人事'],
-            ['id' => 4, 'name' => 'セールス'],
-            ['id' => 5, 'name' => '広報'],
-            ['id' => 6, 'name' => '営業'],
-            ['id' => 7, 'name' => 'フロントエンジニア'],
-            ['id' => 8, 'name' => 'サーバーエンジニア'],
-            ['id' => 9, 'name' => 'SRE'],
-        ]);
-        Job::factory(100)->create();
+        Book::factory(40)->create();
     }
 }
